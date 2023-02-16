a98adbf5d20498d4747303c5bacf8bbc837494a0
Nguyen Lam Son <sonnl.bhsoft@gmail.com>
2023-02-16 16:33:36 +0700

    feat: init

---
+++ src/webhook/webhook.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  ForbiddenException,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
+import { WebhookService } from './webhook.service';
+
+@Controller('webhook')
+export class WebhookController {
+  constructor(private readonly webhookService: WebhookService) {}
+
+  @Post()
+  handleMessage(@Body() body: any) {
+    if (body.object === 'page') {
+      body.entry.forEach((entry: any) => {
+        const webhookEvent = entry.messaging[0];
+        console.log(webhookEvent);
+
+        const senderPsid = webhookEvent.sender.id;
+        console.log('Sender PSID: ' + senderPsid);
+
+        if (webhookEvent.message) {
+          return this.webhookService.handleMessage(
+            senderPsid,
+            webhookEvent.message,
+          );
+        } else {
+          return this.webhookService.handlePostback(
+            senderPsid,
+            webhookEvent.postback,
+          );
+        }
+      });
+    } else {
+      return new NotFoundException('Not Found');
+    }
+  }
+
+  @Get()
+  verifyWebhook(@Query() query) {
+    const mode = query['hub.mode'];
+    const token = query['hub.verify_token'];
+    const challenge = query['hub.challenge'];
+    if (mode && token) {
+      if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+        console.log('WEBHOOK_VERIFIED');
+        return challenge;
+      } else {
+        return new ForbiddenException('Forbidden');
+      }
+    }
+  }
+}
---
+++ src/webhook/webhook.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+@Injectable()
+export class WebhookService {
+  constructor(private readonly http: HttpService) {}
+
+  handleMessage(senderPsid, receivedMessage) {
+    let response: any;
+
+    if (receivedMessage.text) {
+      response = {
+        text: `You sent the message: "${receivedMessage.text}". Now send me an image!`,
+      };
+    } else if (receivedMessage.attachments[0].payload.url) {
+      const attachmentUrl = receivedMessage.attachments[0].payload.url;
+      response = {
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            elements: [
+              {
+                title: 'Is this the right picture?',
+                subtitle: 'Tap a button to answer.',
+                image_url: attachmentUrl,
+                buttons: [
+                  {
+                    type: 'postback',
+                    title: 'Yes!',
+                    payload: 'yes',
+                  },
+                  {
+                    type: 'postback',
+                    title: 'No!',
+                    payload: 'no',
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      };
+    }
+    return this.callSendAPI(senderPsid, response);
+  }
+
+  handlePostback(senderPsid, receivedPostback) {
+    let response;
+
+    const payload = receivedPostback.payload;
+    if (payload === 'yes') {
+      response = { text: 'Thanks!' };
+    } else if (payload === 'no') {
+      response = { text: 'Oops, try sending another image.' };
+    }
+  }
+
+  callSendAPI(senderPsid, response) {
+    try {
+      const request_body = {
+        recipient: {
+          id: senderPsid,
+        },
+        message: response,
+      };
+
+      return this.http.post('https://graph.facebook.com/v2.6/me/messages', {
+        params: {
+          access_token: process.env.PAGE_ACCESS_TOKEN,
+        },
+        data: request_body,
+      });
+    } catch (error) {
+      console.error('Unable to send message:' + error);
+    }
+  }
+}
